fbfd1bf616b385a44190883175da2bc2d4b53100
daffanauvan <dfnauvan@gmail.com>
2025-01-09 21:51:17 +0700

    Add files via upload

---
+++ api/db_config.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "basicweb";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db);
+
+
+?>
